90b9da1fe9a4e6835eccfedc413d03224210cdb7
Vinicius Maciel <viniciusfre@gmail.com>
2019-06-11 15:27:07 -0300

    Added GPIO code for light_sleep example

--- examples/system/light_sleep/main/light_sleep_example_main.c
+++ examples/system/light_sleep/main/light_sleep_example_main.c
@@ -18,14 +18,35 @@
 #include "esp_log.h"
 #include "esp32/rom/uart.h"
 #include "driver/rtc_io.h"
+#include "driver/gpio.h"
 
 /* Most development boards have "boot" button attached to GPIO0.
  * You can also change this to another pin.
  */
-#define BUTTON_GPIO_NUM_DEFAULT     0
+#define BUTTON_GPIO_NUM_DEFAULT    39// 0
 
 /* "Boot" button on GPIO0 is active low */
-#define BUTTON_WAKEUP_LEVEL_DEFAULT     0
+#define BUTTON_WAKEUP_LEVEL_DEFAULT     1 //0
+
+#define GPIO_OUTPUT_IO_0    0
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0))
+
+void gpioConfig()
+{
+    gpio_config_t io_conf;
+    //disable interrupt
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+}
 
 void app_main()
 {
@@ -39,21 +60,26 @@ void app_main()
     ESP_ERROR_CHECK(gpio_config(&config));
     gpio_wakeup_enable(button_gpio_num,
             wakeup_level == 0 ? GPIO_INTR_LOW_LEVEL : GPIO_INTR_HIGH_LEVEL);
+    //
+    gpioConfig();
 
     while (true) {
-        /* Wake up in 2 seconds, or when button is pressed */
-        esp_sleep_enable_timer_wakeup(2000000);
+        /* Wake up in 10 seconds, or when button is pressed */
+        esp_sleep_enable_timer_wakeup(10000000);
         esp_sleep_enable_gpio_wakeup();
 
         /* Wait until GPIO goes high */
-        if (rtc_gpio_get_level(button_gpio_num) == wakeup_level) {
+        /*if (rtc_gpio_get_level(button_gpio_num) == wakeup_level) {
             printf("Waiting for GPIO%d to go high...\n", button_gpio_num);
             do {
                 vTaskDelay(pdMS_TO_TICKS(10));
             } while (rtc_gpio_get_level(button_gpio_num) == wakeup_level);
-        }
+        }*/
+        gpio_set_level(GPIO_NUM_0, 0);
+        vTaskDelay(pdMS_TO_TICKS(10000));
 
         printf("Entering light sleep\n");
+        gpio_set_level(GPIO_NUM_0, 1);
         /* To make sure the complete line is printed before entering sleep mode,
          * need to wait until UART TX FIFO is empty:
          */
